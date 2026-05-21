2775524eafbf491320ab31ac09b48ce0892426a5
1andrey1-coder <112785732+1andrey1-coder@users.noreply.github.com>
2026-05-22 08:28:30 +1000

    Update geometry.c

--- 4 лаб/geometry.c
+++ 4 лаб/geometry.c
@@ -5,7 +5,7 @@ double getPerimeter(double a, double b, double c) {
     return a + b + c;
 }
 
-double getArea(double a, double b, double c) {
+double getAs(double a, double b, double c) {
     double p = getPerimeter(a, b, c) / 2.0;
     return sqrt(p * (p - a) * (p - b) * (p - c));
 }
